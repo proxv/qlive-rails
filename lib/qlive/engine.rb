1c9aa1bab010a7b684cb27949ebd1b164d1db260
Jeff Ferber <jeff@proxv.com>
2012-07-25 20:22:15 -0700

    fix default base path

--- lib/qlive/engine.rb
+++ lib/qlive/engine.rb
@@ -6,12 +6,14 @@ module Qlive
   class Engine < Rails::Engine
 
     initializer "qlive" do |app|
+      Qlive.setup[:base_path] ||= "#{Rails.root}/spec/qunits"
+
       puts "Mounting Qlive::Rack to allow qunit testing against your server's backend. (Never use this rack on production systems.)"
       if Qlive.setup[:hand_mounted]
         Qlive.setup[:hand_mounted].call
       else
         app.middleware.insert_after(::ActionDispatch::ParamsParser, Qlive::Rack, {
-          :base_path => "#{Rails.root}/spec/qunits"
+          :base_path => Qlive.setup[:base_path]
         })
         app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
